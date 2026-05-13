218753eb1c7f2c301970eab77f51f8f2c811b1a0
skerishKang <198730@jnu.ac.kr>
2026-05-13 16:15:48 +0900

    refactor(#201): harden HTML escaping helper (#202)

--- public/app-utils.js
+++ public/app-utils.js
@@ -44,7 +44,8 @@ function escapeHtml(value) {
     .replaceAll("&", "&amp;")
     .replaceAll("<", "&lt;")
     .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;");
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
 }
 
 function byBucket(plans, bucket) {
